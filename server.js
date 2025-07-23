89eb497219d6cd7e32e147091cdf4c4d33e23e90
Meheret17 <miheretalemu17@gmail.com>
2025-07-24 00:45:20 +0300

    updated main

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ connectDB();
 app.use('/api', require('./routes/auth'));
 app.use('/api', require('./routes/tasks'));
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Welcome to Task Manager API');    
+});
 //start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
